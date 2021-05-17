cce87c182585d5ea910dd990e8ecfd0613fe6357
Renato Esposito <renato.esposito1999@outlook.com>
2021-05-17 19:07:28 +0200

    Page Accedi.html and register's form are complete

--- Javascript/Global.js
+++ Javascript/Global.js
@@ -1,9 +1,9 @@
 function reSizeFooter(){
     if (window.screen.width < 768) {
 
-        let elems = document.getElementsByClassName('_reSizeFooter');
+        let elems = document.getElementsByClassName('_reSizeFooter')
         for (let i=0;i<elems.length;i+=1){
-            elems[i].style.display = 'none';
+            elems[i].style.display = 'none'
         }
 
 
@@ -20,9 +20,30 @@ reSizeFooter()
 function reSizeLogo(){
     if(window.screen.width<426){
         console.log("TEST")
-        let elems = document.getElementById("_logo");
+        let elems = document.getElementById("_logo")
         elems.src="../IMG/logo2.png"
     }
 }
 
 reSizeLogo()
+
+
+function registrati(){
+    //Cambia anche l'elemento content di meta
+    let elems = document.getElementById("nascondi")
+        elems.classList.remove("fadeIn2")
+        elems.classList.add("fadeOutRightBig")
+
+    setTimeout(function(){
+        elems.parentNode.removeChild(elems)
+
+        elems = document.getElementById("registrazione")
+        elems.classList.remove("_ds-none")
+        elems.classList.add("fadeInLeftBig")
+    }, 300);
+
+    let _desc="Pagina di registrazione per NotMiss"
+    document.querySelector('meta[name="description"]').setAttribute("content", _desc);
+}
+
+
