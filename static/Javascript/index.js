63b7d116fb1ccbbb245d4d1a1d457bec3558c9c4
Renato Esposito <renato.esposito1999@outlook.com>
2021-06-10 18:40:27 +0200

    Changes

--- static/Javascript/index.js
+++ static/Javascript/index.js
@@ -1,3 +1,6 @@
+let obj
+let array = new Array()
+
 function resizeTextArea() {
     if (window.screen.width < 768 && window.screen.width > 328 ){
         $(".text-area").attr("cols", "10")
@@ -14,13 +17,14 @@ function resizeTextArea() {
 
 }
 
-
 //ele1 è l'elemento a cui aggiungere la classe
 //ele2 è l'elemento a cui eliminare la classe.
 function querySelector(ele1,ele2){
     $(ele1).addClass("_selected")
     $(ele2).removeClass("_selected")
 }
+
+
 
 $('#_priv').click(function(){
     querySelector('#_priv','#_pub')
@@ -39,11 +43,17 @@ function loadEventi(){
         url:'/loadEventi',
         type:'POST',
         success: function (result){
+
             for (let i = 0; i < result.length; i++) {
                 obj = result[i]
-                let newDiv
-                    $("#acaso").attr('id', obj['_id']).text(obj['nome'])
-                $('#contents').append(newDiv)
+                let newDiv = '<!-- Inizio Post --> <div class="container mt-2 mb-5"> <div class="postContainer"> <div class="row bg-dark" style="border-radius: 10px"> <div class="col-12 text-center text-white"> <h3> <span class="nome"></span></h3> </div> </div> <div class="row text-center"> <div class="col-4"><p class=\'nomeCreatore\' class="ml-2"></p></div> <div class="col-4"><p class="luogo"></p></div> <div class="col-4"><p class="tipologia"></p></div> </div> <div class="row"> <div class="col-2"> <p style="text-align: center"> <img class="_id mostraAltro" src="../static/IMG/Icons/buttonInfo256x256.png" width="32px" height="32px" alt="info"> </p> </div> <div class="col-8 text-center"> <p class="dataI text-center"></p> </div> <div class="col-2 " style="color: dodgerblue; font-weight: normal"> <p style="text-align: center"> <img src="../static/IMG/Icons/addButton256x256.png" alt="Partecipa" width="32px" height="32px"> </p> </div> </div> </div> </div> <!-- fine post -->'
+                $("#contents").append(newDiv)
+                $("._id").attr('class', "_"+obj['_id'])
+                array.push("_" + obj['_id'])
+                $(".nome").attr('class', obj['nome']).text(obj['nome'])
+                $(".luogo").attr('class', obj['luogo']).text(obj['luogo'])
+                $(".nomeCreatore").attr('class', obj['nomeCreatore']).text(obj['nomeCreatore'] + " " + obj['cognomeCreatore'])
+                $(".tipologia").attr('class', obj['tipologia']).text(obj['tipologia'])
             }
         }
 
@@ -56,6 +66,11 @@ function close2() {
     $("#copri").removeClass("hide")
 }
 
+function open2() {
+    $("#_divInfo").removeClass("_ds-none")
+    $("#_divInfo").addClass("d-block")
+    $("#copri").addClass("hide")
+}
 
 $(document).ready(function (){
     loadEventi();
@@ -64,10 +79,19 @@ $(document).ready(function (){
     $('#_pub').click(loadEventi)
     $('#_priv').click(loadEventi)
 
-    $("#mostraAltro").click(function () {
-    $("#_divInfo").removeClass("_ds-none")
-    $("#_divInfo").addClass("d-block")
-    $("#copri").addClass("hide")
+    $(".mostraAltro").click(open2)
+
+    //cerco la classe dell'elemento cliccato nell'array che contiene tutti gli id dei post, se lo trovo allora apro il div con maggiori info, inserendo tutti i dettagli gia contenuti in obj.
+    $(document).click(function (e){
+        let classList = e.target.className
+        for (let i = 0; i < array.length; i++) {
+            if (classList.indexOf(array[i]) != -1){
+                //ho trovato l'elemento cliccato
+                open2()
+                //inserisco le informazioni
+                newDivInfo()
+            }
+        }
     })
 
     $(document).click(function (e){
@@ -77,7 +101,5 @@ $(document).ready(function (){
 
 
 })
-
-
 
 
